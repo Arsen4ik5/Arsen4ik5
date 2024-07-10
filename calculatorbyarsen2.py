bb971636a05b84ad51b693c4bdacccd3ea85b311
Arsen4ik5 <arsenakount1000@gmail.com>
2024-07-10 13:48:57 +0500

    Add files via upload

---
+++ calculatorbyarsen2.py
@@ -0,0 +1,12 @@
+print("𝙲𝙰𝙻𝙲𝚄𝙻𝙰𝚃𝙾𝚁 𝙱𝚈 𝙰𝚁𝚂𝙴𝙽 ") 
+
+num1 = int(input("ЧИСЛО N1: "))
+num2 = int(input("ЧИСЛО N2: "))
+num3 = num1 + num2
+num4 = num1 - num2
+num5 = num1 / num2
+num6 = num1 * num2
+print('+:',num3)
+print('-:',num4)
+print('/:',num5)
+print('*:',num6)
